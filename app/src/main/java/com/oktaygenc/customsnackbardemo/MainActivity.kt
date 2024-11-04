47b7e3586a674393f6b6506d0bdc629b0644ff40
Oktay Genc <gncoktay3866@gmail.com>
2024-11-04 18:43:46 +0300

    feat: Update MainActivity to integrate custom Snackbar functionality and enable edge-to-edge display

--- app/src/main/java/com/oktaygenc/customsnackbardemo/MainActivity.kt
+++ app/src/main/java/com/oktaygenc/customsnackbardemo/MainActivity.kt
@@ -1,23 +1,37 @@
 package com.oktaygenc.customsnackbardemo
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 import com.oktaygenc.customsnackbardemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding.showSnackbarButton.setOnClickListener {
+            showCustomSnackbar()
+        }
     }
+    private fun showCustomSnackbar() {
+        showCustomSnackBar("Custom Snackbar Message", binding.main)
+    }
+
+
 }
